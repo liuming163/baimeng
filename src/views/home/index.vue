8d3c11d9ada6b3dd06df8ae37d7a2a6a995b0d0f
liuming163 <lm18602904067@163.com>
2025-03-12 17:37:35 +0800

    首页设置swiper滑动

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,55 @@
 <template>
-  <div>
+  <div class="home">
     <!-- 首页 -->
-    home
+    <swiper
+      :slides-per-view="1"
+      :direction="'vertical'"
+      pagination
+      class="swiper-box"
+    >
+      <swiper-slide>
+        <swiper :slides-per-view="1" pagination class="swiper-box">
+          <swiper-slide v-for="slide in slides" :key="slide.id">
+            <img :src="slide.image" :alt="slide.alt" />
+          </swiper-slide>
+        </swiper>
+      </swiper-slide>
+      <swiper-slide>
+        <img src="https://image.veseo.cn/v3kjdc4xcv90ia6o2s" alt="" />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="https://image.veseo.cn/im3m8f3xyp9hfzzvx9" alt="" />
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/swiper-bundle.css'
+
+const slides = ref([
+  { id: 1, image: 'https://image.veseo.cn/gx5jesf9qty5vprnzb', alt: 'Slide 1' },
+  { id: 2, image: 'https://image.veseo.cn/v3kjdc4xcv90ia6o2s', alt: 'Slide 2' },
+  { id: 3, image: 'https://image.veseo.cn/im3m8f3xyp9hfzzvx9', alt: 'Slide 3' },
+])
+</script>
+
+<style lang="scss" scoped>
+.home {
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+
+  .swiper-box {
+    width: 100%;
+    height: 100%;
+
+    img {
+      width: 100%;
+      object-fit: cover;
+    }
+  }
+}
+</style>
