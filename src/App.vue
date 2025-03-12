42d06e21063c187755abe33858c49123d4731d3d
liuming163 <lm18602904067@163.com>
2025-03-12 17:36:18 +0800

    header页面添加固定定位

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
-  <div>
-    <Header></Header>
+  <div class="app">
+    <Header class="header"></Header>
+    <router-view></router-view>
   </div>
-
-  <router-view></router-view>
 </template>
 
 <script setup>
-import Header from '@/components/Header.vue';
+import Header from '@/components/Header.vue'
 </script>
 
 <style lang="scss">
 @import '@/assets/styles/common.scss';
+
+.app {
+  position: relative;
+
+  .header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 100;
+  }
+}
 </style>
