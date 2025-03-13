c5017c3d8dc18696030b9de9bd2900ffa239f099
liuming163 <lm18602904067@163.com>
2025-03-13 16:08:17 +0800

    完成page3静态页面的绘制

--- src/views/home/components/HomePage3.vue
+++ src/views/home/components/HomePage3.vue
@@ -8,11 +8,102 @@
         无论是创意、设计、还是体验，我们都力求颠覆传统，在每一个细节中做到完美极致！
       </div>
     </div>
-    <div class="inner"></div>
+    <div class="inner">
+      <swiper :slides-per-view="1" pagination class="swiper-box">
+        <swiper-slide v-for="slide in slides" :key="slide.id">
+          <div class="swiper-box-content">
+            <div
+              v-for="item in slide.content"
+              :key="item.id"
+              class="content-item"
+            >
+              <img :src="item.image" />
+              <p class="item-title">{{ item.title }}</p>
+            </div>
+          </div>
+        </swiper-slide>
+      </swiper>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/swiper-bundle.css'
+
+const slides = ref([
+  {
+    id: 1,
+    content: [
+      {
+        id: 11,
+        image: 'https://image.veseo.cn/gx5jesf9qty5vprnzb',
+        alt: 'Slide 1',
+        title: '恩喜整形-桃花姬主题名媛会11',
+      },
+      {
+        id: 12,
+        image: 'https://image.veseo.cn/v3kjdc4xcv90ia6o2s',
+        alt: 'Slide 2',
+        title: '恩喜整形-桃花姬主题名媛会12',
+      },
+      {
+        id: 13,
+        image: 'https://image.veseo.cn/im3m8f3xyp9hfzzvx9',
+        alt: 'Slide 3',
+        title: '恩喜整形-桃花姬主题名媛会13',
+      },
+    ],
+  },
+  {
+    id: 2,
+    content: [
+      {
+        id: 21,
+        image: 'https://image.veseo.cn/gx5jesf9qty5vprnzb',
+        alt: 'Slide 1',
+        title: '恩喜整形-桃花姬主题名媛会21',
+      },
+      {
+        id: 22,
+        image: 'https://image.veseo.cn/v3kjdc4xcv90ia6o2s',
+        alt: 'Slide 2',
+        title: '恩喜整形-桃花姬主题名媛会22',
+      },
+      {
+        id: 23,
+        image: 'https://image.veseo.cn/im3m8f3xyp9hfzzvx9',
+        alt: 'Slide 3',
+        title: '恩喜整形-桃花姬主题名媛会23',
+      },
+    ],
+  },
+  {
+    id: 3,
+    content: [
+      {
+        id: 31,
+        image: 'https://image.veseo.cn/gx5jesf9qty5vprnzb',
+        alt: 'Slide 1',
+        title: '恩喜整形-桃花姬主题名媛会31',
+      },
+      {
+        id: 32,
+        image: 'https://image.veseo.cn/v3kjdc4xcv90ia6o2s',
+        alt: 'Slide 2',
+        title: '恩喜整形-桃花姬主题名媛会32',
+      },
+      {
+        id: 33,
+        image: 'https://image.veseo.cn/im3m8f3xyp9hfzzvx9',
+        alt: 'Slide 3',
+        title: '恩喜整形-桃花姬主题名媛会33',
+      },
+    ],
+  },
+])
+</script>
 
 <style lang="scss" scoped>
 .home-page3 {
@@ -28,7 +119,6 @@
     min-height: 30%;
     font-size: 24px;
     text-align: center;
-    // background-color: purple;
     color: #767676;
 
     h3 {
@@ -56,6 +146,46 @@
     width: 100%;
     height: 60%;
     margin-top: 4%;
+
+    .swiper-box {
+      width: 100%;
+      height: 100%;
+
+      .swiper-box-content {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: space-around;
+        cursor: pointer;
+
+        .content-item {
+          width: 30%;
+          height: 100%;
+          margin: 0 5px;
+
+          img {
+            width: 100%;
+            object-fit: cover;
+          }
+
+          .item-title {
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            padding: 20px 0;
+            position: relative;
+            width: 90%;
+            text-align: center;
+            background-color: #fff;
+            top: -50px;
+            margin: 0 auto;
+            font-size: 24px;
+            box-shadow: 0px 2px 11px -2px var(--active-color);
+            -webkit-box-shadow: 0px 2px 11px -2px var(--active-color);
+          }
+        }
+      }
+    }
   }
 }
 </style>
