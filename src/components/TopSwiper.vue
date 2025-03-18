697cfe051fb4468e37873d222face3c136f8e242
liuming163 <lm18602904067@163.com>
2025-03-18 11:55:32 +0800

    搭建顶部通用swiper

---
+++ src/components/TopSwiper.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="top-swiper">
+    <!-- 顶部通用swiper -->
+    <el-carousel>
+      <el-carousel-item v-for="item in swiperList" :key="item.id">
+        <img :src="item.img" alt="" />
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const swiperList = ref([
+  {
+    id: 1,
+    img: 'http://image.veseo.cn/6ffefljyta2tsflmhc',
+  },
+  {
+    id: 2,
+    img: 'http://image.veseo.cn/6ffefljyta2tsflmhc',
+  },
+])
+</script>
+
+<style lang="scss" scoped>
+.top-swiper {
+  width: 100%;
+  padding-top: 120px;
+
+  img {
+    width: 100%;
+    vertical-align: middle;
+  }
+}
+</style>
